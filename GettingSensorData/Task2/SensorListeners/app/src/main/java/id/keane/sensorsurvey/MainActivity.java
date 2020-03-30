c8c11f67d0711da517207810a3aaa5c19b1dce5d
Jason Keane <jason@keane.id>
2020-03-30 18:31:29 +0100

    Add task 2

---
+++ GettingSensorData/Task2/SensorListeners/app/src/main/java/id/keane/sensorsurvey/MainActivity.java
@@ -0,0 +1,97 @@
+package id.keane.sensorsurvey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mSensorProximity;
+    private Sensor mSensorLight;
+
+    private TextView mTextSensorLight;
+    private TextView mTextSensorProximity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        mTextSensorLight = (TextView) findViewById(R.id.label_light);
+        mTextSensorProximity = (TextView) findViewById(R.id.label_proximity);
+
+        mSensorProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        String sensor_error = getResources().getString(R.string.error_no_sensor);
+
+        if (mSensorLight == null) {
+            mTextSensorLight.setText(sensor_error);
+        }
+
+        if (mSensorProximity == null) {
+            mTextSensorProximity.setText(sensor_error);
+        }
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        if (mSensorProximity != null) {
+            mSensorManager.registerListener(this, mSensorProximity,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorLight != null) {
+            mSensorManager.registerListener(this, mSensorLight,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        // unregister listener to save power when app is not in foreground
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int sensorType = event.sensor.getType();
+        float currentValue = event.values[0];
+
+        switch (sensorType) {
+            case Sensor.TYPE_LIGHT:
+                // handle light sensor
+                mTextSensorLight.setText(getResources().getString(
+                        R.string.label_light, currentValue));
+                break;
+            case Sensor.TYPE_PROXIMITY:
+                mTextSensorProximity.setText(getResources().getString(
+                        R.string.label_proximity, currentValue));
+                break;
+            default:
+                // do nothing
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // light and proximity sensors do not report accuracy changes
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
